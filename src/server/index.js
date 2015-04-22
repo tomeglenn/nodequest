e013e93f917c40d44b0679d34bd9ba500b3c28a0
TomMiimax <tom@miimax.com>
2015-04-22 21:11:19 +0100

    Implemented a basic node server

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,17 @@
-console.log('Hello, server!');
+var app = require('http').createServer();
+var io = require('socket.io').listen(app);
+
+var port = process.env.port || 3000;
+app.listen(port);
+
+console.log('Listening on localhost:' + port);
+
+io.on('connect', function(socket) {
+  console.log('client connected: ' + socket.id);
+
+  io.broadcast.emit('connected', socket.id);
+
+  socket.on('disconnect', function() {
+    console.log('client disconnected: ' + socket.id);
+  });
+});
