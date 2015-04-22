3db99788ca758fcde164a3e48d0017eae3e8a68e
TomMiimax <tom@miimax.com>
2015-04-22 22:35:49 +0100

    Another attempt to make it work on heroku

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,14 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server, { origins:'*:*' });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 server.listen(port);
 console.log('Listening on localhost:' + port);
 
