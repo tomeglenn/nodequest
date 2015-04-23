54a37e9b4700660271926203b84543ce76a467e3
Tom Glenn <tom.glenn@stormid.com>
2015-04-23 14:42:27 +0100

    Began implementing connect/disconnect login

--- src/client/game/index.js
+++ src/client/game/index.js
@@ -1,16 +1,70 @@
-var game = new Phaser.Game(640, 480, Phaser.AUTO, 'game', {
+var game = new Phaser.Game(640, 480, Phaser.AUTO, 'game');
+
+var mainState = {
   preload: function() {
-
+    game.load.image('ball', '/game/assets/sprites/ball.png');
   },
+
   create: function() {
-
+    var connectionHandler = new ConnectionHandler();
   },
+
   update: function() {
 
   }
-});
+}
 
-var socket = io('http://server-nodequest.herokuapp.com');
-socket.on('connect', function() {
-  alert('connected');
-});
+game.state.add('main', mainState);
+game.state.start('main');
+
+/* CONNECTION HANDLER */
+var ConnectionHandler = function () {
+  this.socket = io('localhost:3000');
+  this.players = [];
+  this.init();
+};
+
+ConnectionHandler.prototype.init = function() {
+  this.socket.on('player_joined', function(playerData) {
+    addPlayerSprite(playerData);
+  });
+
+  this.socket.on('game_state', function(playersData) {
+    for(var i = 0; i < playersData.length; i++) {
+      addPlayerSprite(playersData[i]);
+    }
+  });
+
+  this.socket.on('player_quit', function(playerId) {
+    var player = undefined;
+    for(var i = 0; i < players.length; i++) {
+      if (players[i].id == playerId) {
+        players = players[i];
+        players.splice(i, 1);
+      }
+    }
+
+    if (player) {
+      player.destroy();
+    }
+  });
+};
+
+/* PLAYER */
+var Player = function(game, x, y) {
+    Phaser.Sprite.call(this, game, x, y, 'ball');
+}
+
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+Player.prototype.constructor = Player;
+Player.prototype.update = function () {
+
+}
+
+/* HELPER */
+function addPlayerSprite(playerData) {
+  var player = new Player(game, playerData.position.x, playerData.position.y);
+  player.id = playerData.id;
+  game.add.existing(player);
+  players.push(player);
+}
--- src/server/index.js
+++ src/server/index.js
@@ -7,3 +7,47 @@ var io = require('socket.io')(server);
 
 server.listen(port);
 console.log('Listening on localhost:' + port);
+
+
+var players = [];
+
+io.on('connection', function(socket) {
+  console.log('Client Connected { id : ' + socket.id + ' }');
+
+  // Create a new player, tell the client about their own player
+  // and also tell them the current state of the game
+  var playerData = {
+    id : socket.id,
+    isMine : true,
+    position : {
+      x : randomBetween(0, 200),
+      y : randomBetween(0, 200)
+    }
+  };
+
+  socket.emit('player_joined', playerData);
+  socket.emit('game_state', players);
+
+  // Now update the isMine property (we are no longer talking to just the client)
+  // And tell everyone else that a player has joined
+  playerData.isMine = false;
+  socket.broadcast.emit('player_joined', playerData);
+
+  // Add the player to the list of connected players
+  players.push(playerData);
+
+  socket.on('disconnect', function() {
+    for(var i = 0; i < players.length; i++) {
+      if(players[i].id == socket.id) {
+        players.splice(i, 1);
+      }
+    }
+
+    io.emit('player_quit', socket.id);
+  });
+});
+
+
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * ((max-min)+1) + min);
+}
